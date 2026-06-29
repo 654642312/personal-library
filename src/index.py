d83c1b8639b2989723751dde2ed856e8bd82fa5b
654642312 <edgar_belandria010@hotmail.com>
2026-06-29 08:01:58 -0400

    feat: exit function

--- src/index.py
+++ src/index.py
@@ -1,6 +1,9 @@
 
 import questionary
 
+def exit():
+    print("👋 Leaving...")
+
 def listBooks(): 
     books = ["1984", "Cien años de soledad", "Dune", "Fahrenheit 451"]
 
@@ -13,12 +16,13 @@ def listBooks():
             book_selected = questionary.select("Select a book", choices=options).ask()
 
             if book_selected == "-- Exit --" or book_selected is None:
-                print("👋 Leaving...")
+                exit()
                 break
 
 def main():
     option_menu = {
-        "list the books": listBooks
+        "list the books": listBooks,
+        "-- Exit --": exit,
     }
 
     option = questionary.select("Personal Library", choices=list(option_menu.keys())).ask()
