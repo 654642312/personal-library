e23583274b878bf599ac559968658d53926096ab
654642312 <edgar_belandria010@hotmail.com>
2026-06-29 07:58:46 -0400

    feat: list of the books

--- src/index.py
+++ src/index.py
@@ -1,7 +1,33 @@
 
 import questionary
+
+def listBooks(): 
+    books = ["1984", "Cien años de soledad", "Dune", "Fahrenheit 451"]
+
+    while True:
+        if not books:
+            print("no books")
+        else:
+            options = books + ["-- Exit --"]
+
+            book_selected = questionary.select("Select a book", choices=options).ask()
+
+            if book_selected == "-- Exit --" or book_selected is None:
+                print("👋 Leaving...")
+                break
+
 def main():
-    option = questionary.select("Personal Library", choices=["List books", "Add new book"]).ask()
+    option_menu = {
+        "list the books": listBooks
+    }
+
+    option = questionary.select("Personal Library", choices=list(option_menu.keys())).ask()
+
+    if option in option_menu:
+        option_selected = option_menu[option]
+        option_selected()
+
+
 
 if __name__ == '__main__':
     main()
